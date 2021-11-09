b020ecafaccf73237267c23364b377a79e9e5c51
Alexander <undergrounder16@gmail.com>
2021-11-09 13:35:21 -0500

    agregando loader

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <router-view/>
+  <div class="w-screen h-screen">
+    <loading-screen :is-loading="isLoading"></loading-screen>
+    <router-view v-if="!isLoading"/>
+  </div>
 </template>
+<script>
+import Loader from "@/components/Common/Loader";
+
+export default {
+  components: {
+    "loading-screen": Loader,
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 5000);
+  },
+};
+</script>
---
+++ src/components/Common/Loader.vue
@@ -0,0 +1,81 @@
+<template>
+  <div v-once></div>
+</template>
+
+<script>
+import { pleaseWait } from "please-wait";
+import "please-wait/build/please-wait.css";
+export default {
+  name: "Loader",
+  props: ["isLoading"],
+  watch: {
+    isLoading: {
+      handler(isLoading) {
+        if (isLoading) {
+          this.open();
+        } else {
+          this.close();
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    open() {
+      // Attaching a `pleaseWaitInstance` property (no need to declare)...
+      if (!this.pleaseWaitInstance) {
+        this.pleaseWaitInstance = pleaseWait({
+          logo: require("@/assets/img/logoHome.gif"),
+          backgroundColor: "#fff",
+        });
+      }
+    },
+    close() {
+      if (this.pleaseWaitInstance) {
+        this.pleaseWaitInstance.finish();
+        this.pleaseWaitInstance = null;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.loading-message {
+  color: white;
+}
+.pg-loading-screen .pg-loading-center-middle {
+  width: 100vw;
+  height: 100vh;
+  display: block !important;
+}
+
+.pg-loading-center-middle h1 {
+  width: 100vw;
+  height: 100vh;
+  display: flex !important;
+  justify-content: center;
+  align-items: center;
+}
+
+.pg-loading-center-middle h1 img {
+  transform: scale(1.3);
+}
+
+.pg-loading-screen.pg-loading .pg-loading-logo-header,
+.pg-loading-screen.pg-loading .pg-loading-html:not(.pg-loaded) {
+  animation: zoom-out 3s cubic-bezier(0.7, 0, 0.3, 1) both !important;
+}
+
+@keyframes zoom-out {
+  0% {
+    opacity: 0;
+
+    transform: scale(1.3);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(0.8);
+  }
+}
+</style>
